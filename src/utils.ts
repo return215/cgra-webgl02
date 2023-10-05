04d4add129ec07873627462269c5946c8f37dccd
return215 <51498719+return215@users.noreply.github.com>
2023-10-05 07:41:23 +0700

    add rotate array utility

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 export const scaleToBottomLeft = (n: number) => (n * 2 - 1);
 export const rgbIntToScale = (n: number) => (n / 255);
 export const showDecimalPoints = (n: number, d: number = 2) => ((Math.round(n * 10**(d)) / 10**(d)).toFixed(d));
+
+// rotate array by n index
+export const rotateArray = (array: number[], n: number) => array.slice(n).concat(array.slice(0, n));
