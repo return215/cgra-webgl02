967a8aacb5afd0921991333cde393ea4ed88c2d3
return215 <51498719+return215@users.noreply.github.com>
2023-10-05 07:52:35 +0700

    Document util functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,11 @@
+/** convert center clip space to bottom left */
 export const scaleToBottomLeft = (n: number) => (n * 2 - 1);
+
+/** scale to 0-1 */
 export const rgbIntToScale = (n: number) => (n / 255);
+
+/** show decimal points up to d */
 export const showDecimalPoints = (n: number, d: number = 2) => ((Math.round(n * 10**(d)) / 10**(d)).toFixed(d));
 
-// rotate array by n index
+/** rotate array by n index */
 export const rotateArray = (array: number[], n: number) => array.slice(n).concat(array.slice(0, n));
